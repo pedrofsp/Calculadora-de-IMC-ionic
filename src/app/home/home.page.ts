028be2c23eaf3f52c66824bbf4b4105e9839fc73
Pedro Sampaio <pedrohenriquefsp90@gmail.com>
2021-03-07 12:46:34 -0300

    Calculate and show IMC

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,15 +7,19 @@ import { ToastController } from "@ionic/angular";
   styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  weight: number;
+  height: number;
+
   constructor(private toastController: ToastController) {}
 
   onCalculate() {
-    console.log("Clicou!");
+    const imc = this.weight / (this.height * this.height);
+    this.showMessage(`IMC = ${imc.toFixed(2)}`);
   }
 
-  async showMessage() {
+  async showMessage(msg: string) {
     const toast = await this.toastController.create({
-      message: "Clicou!",
+      message: msg,
       duration: 3000,
     });
     toast.present();
