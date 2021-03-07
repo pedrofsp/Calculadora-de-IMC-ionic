1cdae0834c7d1d9a1633edb15dd7a90f86247db8
Pedro Sampaio <pedrohenriquefsp90@gmail.com>
2021-03-07 12:27:42 -0300

    Add toast

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ToastController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  constructor(private toastController: ToastController) {}
 
-  constructor() {}
+  onCalculate() {
+    console.log("Clicou!");
+  }
 
+  async showMessage() {
+    const toast = await this.toastController.create({
+      message: "Clicou!",
+      duration: 3000,
+    });
+    toast.present();
+  }
 }
